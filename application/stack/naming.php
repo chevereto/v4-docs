7a536ec9a838a2ef307de7450266d325887a294a
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2024-03-20 15:32:16 -0300

    close #66

--- application/stack/naming.php
+++ application/stack/naming.php
@@ -4,6 +4,6 @@ return [
     'php.md' => 'PHP',
     'mysql-server.md' => 'MySQL Server',
     'web-server.md' => 'Web Server',
-    'cron.md' => '⏲CRON',
+    'cron.md' => 'CRON',
     'operating-system.md' => 'Operating system',
 ];
